1ca650cafbd65e68a4cea9349df321568da6e1f0
Marcelo Ebert <ebert.marcelo@gmail.com>
2025-04-10 00:44:55 -0300

    Add Topic SNS

--- Lambda-FrameSnap-Processor/Function.cs
+++ Lambda-FrameSnap-Processor/Function.cs
@@ -29,7 +29,7 @@ public class Function
     public Function(IAmazonS3 s3Client, HttpClient httpClient)
     {
         BUCKET_NAME = Environment.GetEnvironmentVariable("BUCKET_NAME") ?? "framesnap-video-bucket";
-        API_BASE_URL = Environment.GetEnvironmentVariable("API_BASE_URL") ?? "http://a431b7e3b84cc4560b0a6f6e46f866f7-391439884.us-east-1.elb.amazonaws.com";
+        API_BASE_URL = Environment.GetEnvironmentVariable("API_BASE_URL") ?? "http://a7ab35083428b4f2389d506576ae8224-1198331366.us-east-1.elb.amazonaws.com";
 
         _s3Client = s3Client;
         _httpClient = httpClient;
